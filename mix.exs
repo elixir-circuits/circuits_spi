2628743f9f40f89ce762ad8cd0b3160bdbd62c7b
Mark Sebald <mdsebald@users.noreply.github.com>
2018-11-15 12:04:11 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Circuits.SPI.MixProject do
   def project do
     [
       app: :circuits_spi,
-      version: "0.1.0",
+      version: "0.2.0-dev",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
