6f04a5cf604de19a147ffb5de2ab3e3ad74fa3ea
Frank Hunleth <fhunleth@troodon-software.com>
2025-09-20 14:07:02 -0400

    Fix Elixir 1.19 deprecation warning

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,9 +4,4 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# Always warning as errors
-if Version.match?(System.version(), "~> 1.10") do
-  Code.put_compiler_option(:warnings_as_errors, true)
-end
-
 ExUnit.start()
