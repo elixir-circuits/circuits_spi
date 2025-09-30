7260408d35ba9923ae70695005cfd393ed031149
Frank Hunleth <fhunleth@troodon-software.com>
2025-09-30 17:48:29 -0400

    WIP

---
+++ backends/circuits_spi_linux/test/circuits_spi_linux_test.exs
@@ -0,0 +1,8 @@
+defmodule CircuitsSpiLinuxTest do
+  use ExUnit.Case
+  doctest CircuitsSpiLinux
+
+  test "greets the world" do
+    assert CircuitsSpiLinux.hello() == :world
+  end
+end
--- backends/linux/lib/linux_backend_nif.ex
+++ backends/linux/lib/linux_backend_nif.ex
@@ -11,7 +11,7 @@ defmodule Circuits.SPI.LinuxBackendNIF do
   @compile {:autoload, false}
 
   def load_nif() do
-    :erlang.load_nif(:code.priv_dir(:circuits_spi) ++ ~c"/spi_nif", 0)
+    :erlang.load_nif(:code.priv_dir(:circuits_spi_linux) ++ ~c"/circuits_spi_linux", 0)
   end
 
   def open(_bus_name, _mode, _bits_per_word, _speed_hz, _delay_us, _lsb_first),
