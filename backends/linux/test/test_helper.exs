f776b5d1233a16a4aa7c88d6d4f89f6dab0c8b78
Frank Hunleth <fhunleth@troodon-software.com>
2025-10-06 11:56:44 -0400

    mix format

--- backends/linux/test/test_helper.exs
+++ backends/linux/test/test_helper.exs
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 if not File.exists?("/dev/spidev0.0") do
-  IO.puts("WARNING: Skipping tests that require an accessible SPI device with MISO and MOSI connected. See README_TESTING.md.")
+  IO.puts(
+    "WARNING: Skipping tests that require an accessible SPI device with MISO and MOSI connected. See README_TESTING.md."
+  )
+
   ExUnit.configure(exclude: :spidev)
 end
 
